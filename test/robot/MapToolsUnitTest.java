1defc68dc3d6b8650a3b495d102f87d3a2f242cc
florian <florian.barrois25@orange.fr>
2013-12-31 00:11:05 +0100

    Merge branch 'master' of /home/florian/Documents/S3/test/Chenot-Barrois with conflicts.

--- test/robot/MapToolsUnitTest.java
+++ test/robot/MapToolsUnitTest.java
@@ -3,6 +3,9 @@ package robot;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
 public class MapToolsUnitTest
 {
     @Test
@@ -12,6 +15,12 @@ public class MapToolsUnitTest
         Assert.assertEquals(0, posfinale.getX());
         Assert.assertEquals(1, posfinale.getY());
     }
+    @Test
+    public void testCounterclockwise1()
+    {
+        Direction direction = Direction.NORTH;
+        Assert.assertEquals(Direction.WEST, MapTools.counterclockwise(direction));
+    }
 
     @Test
     public void testNextForwardPositionSouth() {
@@ -20,6 +29,12 @@ public class MapToolsUnitTest
         Assert.assertEquals(0, posfinale.getX());
         Assert.assertEquals(-1, posfinale.getY());
     }
+    @Test
+    public void testCounterclockwise2()
+    {
+        Direction direction = Direction.WEST;
+        Assert.assertEquals(Direction.SOUTH, MapTools.counterclockwise(direction));
+    }
 
     @Test
     public void testNextForwardPositionEast() {
@@ -27,6 +42,11 @@ public class MapToolsUnitTest
         Coordinates posfinale = MapTools.nextForwardPosition(posinitiale,Direction.EAST);
         Assert.assertEquals(1, posfinale.getX());
         Assert.assertEquals(0, posfinale.getY());
+    @Test
+    public void testCounterclockwise3()
+    {
+        Direction direction = Direction.SOUTH;
+        Assert.assertEquals(Direction.EAST, MapTools.counterclockwise(direction));
     }
 
     @Test
@@ -67,6 +87,40 @@ public class MapToolsUnitTest
         Coordinates posfinale = MapTools.nextBackwardPosition(posinitiale, Direction.EAST);
         Assert.assertEquals(-1, posfinale.getX());
         Assert.assertEquals(0, posfinale.getY());
+    }
+    @Test
+    public void testCounterclockwise4()
+    {
+        Direction direction = Direction.EAST;
+        Assert.assertEquals(Direction.NORTH, MapTools.counterclockwise(direction));
+    }
+
+    @Test
+    public void testClockwise1()
+    {
+        Direction direction = Direction.NORTH;
+        Assert.assertEquals(Direction.EAST, MapTools.clockwise(direction));
+    }
+
+    @Test
+    public void testClockwise2()
+    {
+        Direction direction = Direction.EAST;
+        Assert.assertEquals(Direction.SOUTH, MapTools.clockwise(direction));
+    }
+
+    @Test
+    public void testClockwise3()
+    {
+        Direction direction = Direction.SOUTH;
+        Assert.assertEquals(Direction.WEST, MapTools.clockwise(direction));
+    }
+
+    @Test
+    public void testClockwise4()
+    {
+        Direction direction = Direction.WEST;
+        Assert.assertEquals(Direction.NORTH, MapTools.clockwise(direction));
     }
 
 
