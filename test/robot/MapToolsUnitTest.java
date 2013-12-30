f8e4f7afa8cf1c9e7f3374f55b616b2507c9331e
Maxime Chenot <maxime.c25@gmail.com>
2013-12-30 23:55:03 +0100

    testNextForwardPosition (toutes directions) et testBackWardPosition (toutes directions)

--- test/robot/MapToolsUnitTest.java
+++ test/robot/MapToolsUnitTest.java
@@ -1,13 +1,73 @@
 package robot;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
 public class MapToolsUnitTest
 {
-    public void testNextForwardPosition()
-    {
-
-
+    @Test
+    public void testNextForwardPositionNorth() {
+        Coordinates posinitiale = new Coordinates(0, 0);
+        Coordinates posfinale = MapTools.nextForwardPosition(posinitiale, Direction.NORTH);
+        Assert.assertEquals(0, posfinale.getX());
+        Assert.assertEquals(1, posfinale.getY());
     }
 
+    @Test
+    public void testNextForwardPositionSouth() {
+        Coordinates posinitiale = new Coordinates(0, 0);
+        Coordinates posfinale = MapTools.nextForwardPosition(posinitiale,Direction.SOUTH);
+        Assert.assertEquals(0, posfinale.getX());
+        Assert.assertEquals(-1, posfinale.getY());
+    }
+
+    @Test
+    public void testNextForwardPositionEast() {
+        Coordinates posinitiale = new Coordinates(0, 0);
+        Coordinates posfinale = MapTools.nextForwardPosition(posinitiale,Direction.EAST);
+        Assert.assertEquals(1, posfinale.getX());
+        Assert.assertEquals(0, posfinale.getY());
+    }
+
+    @Test
+    public void testNextForwardPositionWest() {
+        Coordinates posinitiale = new Coordinates(0, 0);
+        Coordinates posfinale = MapTools.nextForwardPosition(posinitiale,Direction.WEST);
+        Assert.assertEquals(-1, posfinale.getX());
+        Assert.assertEquals(0, posfinale.getY());
+    }
+
+    @Test
+    public void testNextBackwardPositionNorth() {
+        Coordinates posinitiale = new Coordinates(0, 0);
+        Coordinates posfinale = MapTools.nextBackwardPosition(posinitiale, Direction.NORTH);
+        Assert.assertEquals(0, posfinale.getX());
+        Assert.assertEquals(-1, posfinale.getY());
+    }
+
+    @Test
+    public void testNextBackwardPositionSouth() {
+        Coordinates posinitiale = new Coordinates(0, 0);
+        Coordinates posfinale = MapTools.nextBackwardPosition(posinitiale, Direction.SOUTH);
+        Assert.assertEquals(0, posfinale.getX());
+        Assert.assertEquals(1, posfinale.getY());
+    }
+
+    @Test
+    public void testNextBackwardPositionWest() {
+        Coordinates posinitiale = new Coordinates(0, 0);
+        Coordinates posfinale = MapTools.nextBackwardPosition(posinitiale, Direction.WEST);
+        Assert.assertEquals(1, posfinale.getX());
+        Assert.assertEquals(0, posfinale.getY());
+    }
+
+    @Test
+    public void testNextBackwardPositionEast() {
+        Coordinates posinitiale = new Coordinates(0, 0);
+        Coordinates posfinale = MapTools.nextBackwardPosition(posinitiale, Direction.EAST);
+        Assert.assertEquals(-1, posfinale.getX());
+        Assert.assertEquals(0, posfinale.getY());
+    }
 
 
 }
