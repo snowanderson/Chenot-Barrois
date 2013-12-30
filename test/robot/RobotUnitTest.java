d19f10ae4403aa9ecc84c5b023277190028e32ef
florian <florian.barrois25@orange.fr>
2013-12-30 16:26:51 +0100

    Test

--- test/robot/RobotUnitTest.java
+++ test/robot/RobotUnitTest.java
@@ -5,19 +5,29 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static junit.framework.Assert.*;
 import static robot.Direction.*;
 import static robot.Instruction.*;
 import static robot.RoadBookCalculator.calculateRoadBook;
+import robot.UnlandedRobotException;
 
 public class RobotUnitTest {
     private UnlandedRobotException obj;
+    //Robot r;
 
-    @Test
+    @Test//(expected = UnlandedRobotException.class)
     public void testgetXposition()
     {
-        boolean isLanded = true;
-        Assert.assertFalse(""+obj,isLanded);
+        boolean isLanded = false;
+        Assert.assertFalse(isLanded);
+
     }
 
+    @Test ()
+    public void testgetYposition()
+    {
+        boolean isLanded = false;
+        assertTrue("" + obj, isLanded);
+    }
 
 }
